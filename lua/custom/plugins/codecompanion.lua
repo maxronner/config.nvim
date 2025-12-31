61f4356a254399bafab42153f8e458b77961e9fe
Max Ronner <max@ronner.dev>
2025-12-31 13:30:37 +0100

    chore(codecompanion): removed gemini adapter model

--- lua/custom/plugins/codecompanion.lua
+++ lua/custom/plugins/codecompanion.lua
@@ -1,15 +1,14 @@
 return {
   {
     "olimorris/codecompanion.nvim",
-    opts = {
-    },
+    opts = {},
     enabled = true,
     dependencies = {
       "nvim-lua/plenary.nvim",
       "nvim-treesitter/nvim-treesitter",
       "echasnovski/mini.diff",
       {
-        'MeanderingProgrammer/render-markdown.nvim',
+        "MeanderingProgrammer/render-markdown.nvim",
         opts = {
           file_types = { "markdown", "codecompanion" },
         },
@@ -20,10 +19,7 @@ return {
       require("codecompanion").setup({
         interactions = {
           chat = {
-            adapter = {
-              name = "gemini",
-              model = "gemini-flash-latest",
-            }
+            adapter = "gemini",
           },
           inline = {
             adapter = "gemini",
@@ -37,31 +33,51 @@ return {
             gemini = function()
               return require("codecompanion.adapters").extend("gemini", {
                 env = {
-                  api_key = require("custom.passloader").get_var("GEMINI_API_KEY")
+                  api_key = require("custom.passloader").get_var("GEMINI_API_KEY"),
                 },
               })
             end,
             openai = function()
               return require("codecompanion.adapters").extend("openai", {
                 env = {
-                  api_key = require("custom.passloader").get_var("OPENAI_API_KEY")
-                }
+                  api_key = require("custom.passloader").get_var("OPENAI_API_KEY"),
+                },
               })
-            end
-          }
-        }
+            end,
+          },
+        },
       })
-      vim.keymap.set({ "n", "v" }, "<leader>ia", "<cmd>CodeCompanionActions<cr>",
-        { noremap = true, silent = true, desc = "CodeCompanion: Actions" })
-      vim.keymap.set({ "n", "v" }, "<leader>it", "<cmd>CodeCompanionChat Toggle<cr>",
-        { noremap = true, silent = true, desc = "CodeCompanion: Chat Toggle" })
-      vim.keymap.set({ "n", "v" }, "<leader>in", "<cmd>CodeCompanionChat<cr>",
-        { noremap = true, silent = true, desc = "CodeCompanion: Chat New" })
-      vim.keymap.set("v", "<leader>ic", "<cmd>CodeCompanionChat Add<cr>",
-        { noremap = true, silent = true, desc = "CodeCompanion: Chat Add" })
+      vim.keymap.set(
+        { "n", "v" },
+        "<leader>ia",
+        "<cmd>CodeCompanionActions<cr>",
+        { noremap = true, silent = true, desc = "CodeCompanion: Actions" }
+      )
+      vim.keymap.set(
+        { "n", "v" },
+        "<leader>it",
+        "<cmd>CodeCompanionChat Toggle<cr>",
+        { noremap = true, silent = true, desc = "CodeCompanion: Chat Toggle" }
+      )
+      vim.keymap.set(
+        { "n", "v" },
+        "<leader>in",
+        "<cmd>CodeCompanionChat<cr>",
+        { noremap = true, silent = true, desc = "CodeCompanion: Chat New" }
+      )
+      vim.keymap.set(
+        "v",
+        "<leader>ic",
+        "<cmd>CodeCompanionChat Add<cr>",
+        { noremap = true, silent = true, desc = "CodeCompanion: Chat Add" }
+      )
 
-      vim.keymap.set("n", "<leader>ig", ":CodeCompanion /commit<CR>",
-        { noremap = true, silent = true, desc = "CodeCompanion: Generate Git Commit" })
+      vim.keymap.set(
+        "n",
+        "<leader>ig",
+        ":CodeCompanion /commit<CR>",
+        { noremap = true, silent = true, desc = "CodeCompanion: Generate Git Commit" }
+      )
 
       -- Expand 'cc' into 'CodeCompanion' in the command line
       vim.cmd([[cab cc CodeCompanion]])

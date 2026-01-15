1709ae2585099f60835d3ba54198529f16bcc74b
Max Ronner <max@ronner.dev>
2026-01-15 19:26:57 +0100

    feat(plugins): Add kanagawa.nvim and tidy color scheme configs
    Adds 'kanagawa.nvim' to the plugin manager configuration.
    
    Also tidies up spacing and formatting within existing color scheme setups in colors.lua.

--- lua/custom/plugins/colors.lua
+++ lua/custom/plugins/colors.lua
@@ -21,7 +21,7 @@ return {
         invert_tabline = false,
         invert_intend_guides = false,
         inverse = true, -- invert background for search, diffs, statuslines and errors
-        contrast = "",  -- can be "hard", "soft" or empty string
+        contrast = "", -- can be "hard", "soft" or empty string
         palette_overrides = {},
         overrides = {},
         dim_inactive = false,
@@ -33,7 +33,7 @@ return {
     "rose-pine/neovim",
     name = "rose-pine",
     config = function()
-      require('rose-pine').setup({
+      require("rose-pine").setup({
         disable_background = true,
         enable = {
           terminal = true,
@@ -42,49 +42,49 @@ return {
           italic = false,
         },
       })
-    end
+    end,
   },
   {
     "catgoose/nvim-colorizer.lua",
     config = function()
       require("colorizer").setup({
-        filetypes = { "*" },     -- Filetype options.  Accepts table like `user_default_options`
-        buftypes = {},           -- Buftype options.  Accepts table like `user_default_options`
+        filetypes = { "*" }, -- Filetype options.  Accepts table like `user_default_options`
+        buftypes = {}, -- Buftype options.  Accepts table like `user_default_options`
         -- Boolean | List of usercommands to enable.  See User commands section.
-        user_commands = true,    -- Enable all or some usercommands
-        lazy_load = false,       -- Lazily schedule buffer highlighting setup function
+        user_commands = true, -- Enable all or some usercommands
+        lazy_load = false, -- Lazily schedule buffer highlighting setup function
         user_default_options = {
-          names = false,         -- "Name" codes like Blue or red.  Added from `vim.api.nvim_get_color_map()`
-          names_opts = {         -- options for mutating/filtering names.
-            lowercase = true,    -- name:lower(), highlight `blue` and `red`
-            camelcase = true,    -- name, highlight `Blue` and `Red`
-            uppercase = false,   -- name:upper(), highlight `BLUE` and `RED`
+          names = false, -- "Name" codes like Blue or red.  Added from `vim.api.nvim_get_color_map()`
+          names_opts = { -- options for mutating/filtering names.
+            lowercase = true, -- name:lower(), highlight `blue` and `red`
+            camelcase = true, -- name, highlight `Blue` and `Red`
+            uppercase = false, -- name:upper(), highlight `BLUE` and `RED`
             strip_digits = true, -- ignore names with digits,
             -- highlight `blue` and `red`, but not `blue3` and `red4`
           },
           -- Expects a table of color name to #RRGGBB value pairs.  # is optional
           -- Example: { cool = "#107dac", ["notcool"] = "ee9240" }
           -- Set to false to disable, for example when setting filetype options
-          names_custom = false,   -- Custom names to be highlighted: table|function|false
-          RGB = true,             -- #RGB hex codes
-          RGBA = true,            -- #RGBA hex codes
-          RRGGBB = true,          -- #RRGGBB hex codes
-          RRGGBBAA = false,       -- #RRGGBBAA hex codes
-          AARRGGBB = false,       -- 0xAARRGGBB hex codes
-          rgb_fn = false,         -- CSS rgb() and rgba() functions
-          hsl_fn = false,         -- CSS hsl() and hsla() functions
-          css = false,            -- Enable all CSS *features*:
+          names_custom = false, -- Custom names to be highlighted: table|function|false
+          RGB = true, -- #RGB hex codes
+          RGBA = true, -- #RGBA hex codes
+          RRGGBB = true, -- #RRGGBB hex codes
+          RRGGBBAA = false, -- #RRGGBBAA hex codes
+          AARRGGBB = false, -- 0xAARRGGBB hex codes
+          rgb_fn = false, -- CSS rgb() and rgba() functions
+          hsl_fn = false, -- CSS hsl() and hsla() functions
+          css = false, -- Enable all CSS *features*:
           -- names, RGB, RGBA, RRGGBB, RRGGBBAA, AARRGGBB, rgb_fn, hsl_fn
-          css_fn = false,         -- Enable all CSS *functions*: rgb_fn, hsl_fn
+          css_fn = false, -- Enable all CSS *functions*: rgb_fn, hsl_fn
           -- Tailwind colors.  boolean|'normal'|'lsp'|'both'.  True sets to 'normal'
-          tailwind = false,       -- Enable tailwind colors
-          tailwind_opts = {       -- Options for highlighting tailwind names
+          tailwind = false, -- Enable tailwind colors
+          tailwind_opts = { -- Options for highlighting tailwind names
             update_names = false, -- When using tailwind = 'both', update tailwind names from LSP results.  See tailwind section
           },
           -- parsers can contain values used in `user_default_options`
           sass = { enable = false, parsers = { "css" } }, -- Enable sass colors
           -- Highlighting mode.  'background'|'foreground'|'virtualtext'
-          mode = "virtualtext",                           -- Set the display mode
+          mode = "virtualtext", -- Set the display mode
           -- Virtualtext character to use
           virtualtext = "■",
           -- Display virtualtext inline with color.  boolean|'before'|'after'.  True sets to 'after'
@@ -105,8 +105,11 @@ return {
     end,
   },
   {
-    'MeanderingProgrammer/render-markdown.nvim',
-    dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
+    "rebelot/kanagawa.nvim",
+  },
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" }, -- if you use the mini.nvim suite
     -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
     -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
     ---@module 'render-markdown'

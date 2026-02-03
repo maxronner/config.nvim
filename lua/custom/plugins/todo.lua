39bf4723580f3ae40971188d636cfbf77cf8b14e
Max Ronner <max@ronner.dev>
2026-02-03 15:29:33 +0100

    chore(plugins): Remove Avante and Telescope plugins, refactor todo-comments
    Removed `yetone/avante.nvim` and `nvim-telescope/telescope.nvim` plugins, along with their associated configurations (`lua/custom/plugins/avante.lua`, `lua/custom/plugins/telescope.lua`, and `lua/custom/telescope.lua`).
    
    Refactored `folke/todo-comments.nvim` configuration by moving `event = "VeryLazy"` to `cmd` and defining keymaps directly within the `keys` table in the plugin specification, removing the need for an explicit `config` function.
    
    BREAKING CHANGE: The `avante.nvim` and `telescope.nvim` plugins, along with all their features and keymaps, are no longer available in the Neovim configuration.

--- lua/custom/plugins/todo.lua
+++ lua/custom/plugins/todo.lua
@@ -1,37 +1,33 @@
 return {
   {
     "folke/todo-comments.nvim",
-    event = "VeryLazy",
     dependencies = { "nvim-lua/plenary.nvim" },
+    cmd = { "Todo", "TodoTrouble", "TodoFzfLua", "TodoQuickFix", "TodoLocList" },
+    keys = {
+      {
+        "]#",
+        function()
+          require("todo-comments").jump_next()
+        end,
+        desc = "Next todo comment",
+      },
+      {
+        "[#",
+        function()
+          require("todo-comments").jump_prev()
+        end,
+        desc = "Previous todo comment",
+      },
+      {
+        "<leader>ft",
+        "<cmd>TodoFzfLua<cr>",
+        desc = "Fzf: TODOs",
+      },
+    },
     opts = {
       -- your configuration comes here
       -- or leave it empty to use the default settings
       -- refer to the configuration section below
-      keys = {
-        {},
-        {
-          "n",
-          "[#",
-          function()
-            require("todo-comments").jump_prev()
-          end,
-          { desc = "Previous todo comment" },
-        },
-      },
     },
-    config = function()
-      local todo_comments = require("todo-comments")
-      todo_comments.setup()
-
-      vim.keymap.set("n", "]#", function()
-        require("todo-comments").jump_next()
-      end, { desc = "Next todo comment" })
-
-      vim.keymap.set("n", "[#", function()
-        require("todo-comments").jump_prev()
-      end, { desc = "Previous todo comment" })
-
-      vim.keymap.set("n", "<leader>ft", ":TodoFzfLua<CR>", { desc = "Fzf: TODOs" })
-    end,
   },
 }

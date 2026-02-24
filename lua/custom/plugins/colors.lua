8f7abd98c0aac1dbed3d870725eddc35d5fcf0a2
Max Ronner <max@ronner.dev>
2026-02-24 20:55:57 +0100

    chore: Remove unused color schemes
    Removed gruvbox, kanagawa, and tokyonight color schemes from the configuration. This streamlines the color scheme setup, focusing on the rose-pine theme.

--- lua/custom/plugins/colors.lua
+++ lua/custom/plugins/colors.lua
@@ -1,35 +1,5 @@
 return {
   {
-    "ellisonleao/gruvbox.nvim",
-    name = "gruvbox",
-    config = function()
-      require("gruvbox").setup({
-        terminal_colors = true, -- add neovim terminal colors
-        undercurl = true,
-        underline = false,
-        bold = true,
-        italic = {
-          strings = false,
-          emphasis = false,
-          comments = false,
-          operators = false,
-          folds = false,
-        },
-        strikethrough = true,
-        invert_selection = false,
-        invert_signs = false,
-        invert_tabline = false,
-        invert_intend_guides = false,
-        inverse = true, -- invert background for search, diffs, statuslines and errors
-        contrast = "", -- can be "hard", "soft" or empty string
-        palette_overrides = {},
-        overrides = {},
-        dim_inactive = false,
-        transparent_mode = true,
-      })
-    end,
-  },
-  {
     "rose-pine/neovim",
     name = "rose-pine",
     config = function()
@@ -43,17 +13,6 @@ return {
         },
       })
     end,
-  },
-  {
-    "rebelot/kanagawa.nvim",
-  },
-  {
-    "folke/tokyonight.nvim",
-    lazy = false,
-    priority = 1000,
-    opts = {
-      style = "moon", -- The theme comes in three styles, `storm`, a darker variant `night` and `day`
-    },
   },
   {
     "catgoose/nvim-colorizer.lua",
